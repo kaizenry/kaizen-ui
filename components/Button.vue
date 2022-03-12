7ba07764a5ecbbbad4345108a92851f85d81afc8
Eckhardt Dreyer <support@kaizenmedia.co.za>
2022-03-12 11:39:34 +0200

    The world's greatest button

---
+++ components/Button.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+defineProps({
+  color: {
+    type: String,
+    required: false,
+    default: "#2fa",
+  },
+});
+</script>
+
+<template>
+  <button :style="{ backgroundColor: color }">
+    <!-- Dynamic Content -->
+    <slot />
+  </button>
+</template>
